b55917eecd291163997ab5d7ba2f7309bd5bbc93
Kaian <kaian.f.silva@gmail.com>
2021-11-08 22:41:51 -0300

    kaian

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import { stringify } from 'querystring';
 const app = express();
 import 'dotenv/config'
 
+const port = process.env.PORT || 3333
+
+app.listen(port, () => console.log(`Servidor iniciado... ${port}`));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 
 app.use(function(request, response, next) {
@@ -17,9 +22,7 @@ app.get('/', (request: Request, response: Response) => {
 return response.send('OK');
 }); 
 
-const port = process.env.PORT || 3333
 
-app.listen(port, () => console.log(`Servidor iniciado... ${port}`));
 
 
 /* app.use(function(req: Request, res:Response, next) {
